9cc33d13347e049768ffffd21946b22bbcbeaae1
Oleg Gordienov <ogordienov@yandex.ru>
2022-06-16 21:47:47 +0200

    feat: add initial setup mongoose

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+// eslint-disable-next-line no-console
+}).catch((error) => console.log(error));
 
 app.listen(PORT, () => {
   console.log(PORT);
