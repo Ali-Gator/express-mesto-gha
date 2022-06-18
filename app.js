dc86e39ee03d918495355a9b3adf41f788e16414
Oleg Gordienov <ogordienov@yandex.ru>
2022-06-18 20:38:29 +0200

    feat: add routes and controllers to users

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const users = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -9,6 +11,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 // eslint-disable-next-line no-console
 }).catch((error) => console.log(error));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62ae17b5b1124cdfe5ccdd39',
+  };
+  next();
+});
+
+app.use('/users', users);
+
 app.listen(PORT, () => {
-  console.log(PORT);
+
 });
---
+++ controllers/users.js
@@ -0,0 +1,21 @@
+const User = require('../models/users');
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => console.log(err));
+};
+
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => res.status(200).send(user))
+    .catch((err) => console.log(err));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(200).send(user))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
